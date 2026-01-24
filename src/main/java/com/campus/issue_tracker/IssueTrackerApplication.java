78282e686b77c98c7c519940feff5ef1dcb6f323
salitha07 <Salithad889@gmail.com>
2026-01-24 22:01:31 +0530

    added packages

--- src/main/java/com/campus/issue_tracker/IssueTrackerApplication.java
+++ src/main/java/com/campus/issue_tracker/IssueTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CampusIssueAndTrackingSystemApplication {
+public class IssueTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CampusIssueAndTrackingSystemApplication.class, args);
+		SpringApplication.run(IssueTrackerApplication.class, args);
 	}
 
 }
