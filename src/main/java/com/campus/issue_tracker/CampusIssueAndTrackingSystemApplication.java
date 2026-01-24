69f2d913187c1cc287beecd7ed42100acb3d2777
salitha07 <Salithad889@gmail.com>
2026-01-24 21:23:21 +0530

    structure

---
+++ src/main/java/com/campus/issue_tracker/CampusIssueAndTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.campus.issue_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusIssueAndTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusIssueAndTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campus/issue_tracker/CampusIssueAndTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.issue_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusIssueAndTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
