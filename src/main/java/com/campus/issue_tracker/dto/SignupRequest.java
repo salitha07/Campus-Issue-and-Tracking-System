d66a7f2ee9a3bf9064f60fcfaa059c9e7a211dfa
salitha07 <Salithad889@gmail.com>
2026-01-29 13:24:46 +0530

    .....

---
+++ src/main/java/com/campus/issue_tracker/dto/SignupRequest.java
@@ -0,0 +1,24 @@
+package com.campus.issue_tracker.dto;
+
+import com.campus.issue_tracker.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    private Role role; // Role can be ROLE_STUDENT or ROLE_STAFF
+}
